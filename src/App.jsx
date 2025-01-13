6de75063a588ad16584a8e66bf5ccf3f7a5f9d8e
s-nayan <nayansaket3@gmail.com>
2025-01-13 21:44:10 +0530

    changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,9 @@
-import About from "./Components/About/About";
-import Experience from "./Components/Experience/Experience";
-import Footer from "./Components/Footer/Footer";
-import Home from "./Components/Home/Home";
-import Navbar from "./Components/Navbar/Navbar";
-import Projects from "./Components/Projects/Projects";
+import About from "./components/About/About";
+import Experience from "./components/Experience/Experience";
+import Footer from "./components/Footer/Footer";
+import Home from "./components/Home/Home";
+import Navbar from "./components/Navbar/Navbar";
+import Projects from "./components/Projects/Projects";
 
 function App() {
   return (
